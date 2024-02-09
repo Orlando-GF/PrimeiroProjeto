118bc7c09402540cdf0cf802340c8035b7154984
orlando <orlando.gfilho@gmail.com>
2024-02-09 19:49:32 -0300

    initial commit

---
+++ src/br/com/orlando/HelloWorld.java
@@ -0,0 +1,12 @@
+package br.com.orlando;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Meu primeiro código!");
+        System.out.println("Primeiro commit!");
+    }
+}
